1b8f89fe3b7d73d76db9a5ddca8ec35be25b488f
Shadi Barhoumi <shadibarhoumi@gmail.com>
2021-05-31 18:37:15 -0600

    Style changes

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -15,8 +14,10 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
+                <div className="mr-3 text-3xl">
+                  <span role="img" aria-label="Flame emoji logo">
+                    🔥
+                  </span>
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
